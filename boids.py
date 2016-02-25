9c27cac49350928e5be23acec9ed06d48937ae32
Ioana Oprea <ioana.diana.oprea@gmail.com>
2016-02-25 10:50:26 +0000

    Replaced magic numbers with constants

--- boids.py
+++ boids.py
@@ -7,15 +7,20 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Number of boids
-n_boids = 50;
+# Constants
+no_boids = 50;
+move_to_middle_strength = 0.01;
+alert_distance = 100;
+formation_flying_distance = 10000;
+formation_flying_strength = 0.125;
+
 
 # Deliberately terrible code for teaching purposes
 
-boids_x=[random.uniform(-450,50.0) for x in range(n_boids)]
-boids_y=[random.uniform(300.0,600.0) for x in range(n_boids)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(n_boids)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(n_boids)]
+boids_x=[random.uniform(-450,50.0) for x in range(no_boids)]
+boids_y=[random.uniform(300.0,600.0) for x in range(no_boids)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(no_boids)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(no_boids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
@@ -23,22 +28,22 @@ def update_boids(boids):
 	# Fly towards the middle
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
+			xvs[i]=xvs[i]+(xs[j]-xs[i])*move_to_middle_strength/len(xs)
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
+			yvs[i]=yvs[i]+(ys[j]-ys[i])*move_to_middle_strength/len(xs)
 	# Fly away from nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < alert_distance:
 				xvs[i]=xvs[i]+(xs[i]-xs[j])
 				yvs[i]=yvs[i]+(ys[i]-ys[j])
 	# Try to match speed with nearby boids
 	for i in range(len(xs)):
 		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
+			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < formation_flying_distance:
+				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*formation_flying_strength/len(xs)
+				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*formation_flying_strength/len(xs)
 	# Move according to velocities
 	for i in range(len(xs)):
 		xs[i]=xs[i]+xvs[i]
