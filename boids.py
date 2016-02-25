ce83b0f761502e5b951aa723bb1803ea8e606c4d
Ioana Oprea <ioana.diana.oprea@gmail.com>
2016-02-25 21:20:21 +0000

    Added necessary comments

--- boids.py
+++ boids.py
@@ -16,12 +16,13 @@ class Flock(object):
 		return (low_limits[:,np.newaxis] + np.random.rand(2, no_boids)*width[:,np.newaxis])
 
 
+	# Fly towards the middle
 	def fly_middle(self):
 
 		boids_middle=np.mean(self.positions, 1)
 		direction_boids_middle = self.positions-boids_middle[:, np.newaxis]
 		self.velocities -= direction_boids_middle * self.config['move_to_middle_strength']
-
+	# Fly away from nearby boids
 	def fly_away(self):	
 
 		# Compute distances between each boid and the others in a matrix
@@ -35,6 +36,7 @@ class Flock(object):
 		# Move birds that are about to colide
 		self.velocities += np.sum(separate_if_collide,1)
 
+	# Try to match speed with nearby boids
 	def match_speed(self):
 
 		displacements = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
@@ -48,33 +50,28 @@ class Flock(object):
 		# Match speed for those that are going too far
 		self.velocities -=  np.mean(velocity_differences_if_close, 1) * self.config['boids_flying_strength']
 
+	# Move according to velocities
 	def update_position(self):
 		self.positions+=self.velocities
 
+	# Update position of boids
 	def update_boids(self):
 
 		# Fly towards the middle
 	  	self.fly_middle()
-
 	 	# Fly away from nearby boids
 	  	self.fly_away()
-
 	 	# Try to match speed with nearby boids
 	  	self.match_speed()
-
 		# Move according to velocities
 		self.update_position()
 
+	# Set position
 	def set_positions(self, new_position):
 		self.positions = new_position
-
+	# Set velocity
 	def set_velocities(self, new_velocity):
 		self.velocities = new_velocity
-
-	def animate(self,frame):
- 
-	   self.update_boids()
-	   scatter.set_offsets(zip(self.positions[0,:],self.positions[1,:]))
 
 
 if __name__ == "__main__":
