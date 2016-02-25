ea5554788413a8b6cf33f7a592a950901b33afdb
Ioana Oprea <ioana.diana.oprea@gmail.com>
2016-02-25 20:15:32 +0000

    Created class Flock

--- boids.py
+++ boids.py
@@ -1,102 +1,94 @@
-"""
-A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
-for use as an exercise on refactoring.
-"""
-
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 import numpy as np
 import yaml
 
-config=yaml.load(open("config.yaml"))
-
-# Define limits 
-upper_limits = np.array(config['upper_limits'])
-lower_limits = np.array(config['lower_limits'])
-upper_limits_velocities = np.array(config['upper_limits_velocities'])
-lower_limits_velocities = np.array(config['lower_limits_velocities'])
-
-# Function to generate random flock
-def new_formation(no_boids, low_limits, up_limits):
-	width=up_limits-low_limits
-	return (low_limits[:,np.newaxis] + 
-		np.random.rand(2, no_boids)*width[:,np.newaxis])
+class Flock(object):
+	def __init__(self, configuration):
+		self.config=yaml.load(open("config.yaml"))			
+		self.positions = self.new_formation(np.array(self.config['no_boids']), np.array(self.config['lower_limits']), np.array(self.config['upper_limits']))
+		self.velocities = self.new_formation(np.array(self.config['no_boids']), np.array(self.config['lower_limits_velocities']), np.array(self.config['upper_limits_velocities']))
 
 
-# Fly towards the middle
-def fly_middle(positions,velocities):
-
-	boids_middle=np.mean(positions, 1)
-	direction_boids_middle = positions-boids_middle[:, np.newaxis]
-	velocities -= direction_boids_middle * config['move_to_middle_strength']
+	def new_formation(self, no_boids, low_limits, up_limits):
+		width=up_limits-low_limits
+		return (low_limits[:,np.newaxis] + np.random.rand(2, no_boids)*width[:,np.newaxis])
 
 
-# Fly away from nearby boids
-def fly_away(positions,velocities):	
+	def fly_middle(self):
 
-	# Compute distances between each boid and the others in a matrix
-	displacements = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
-	squared_displacements = np.power(displacements,2)
-	square_distances = np.sum(squared_displacements, 0)
-	too_far = square_distances > config['alert_distance']
-	separate_if_collide = np.copy(displacements)
-	separate_if_collide[0,:,:][too_far] = 0
-	separate_if_collide[1,:,:][too_far] = 0
-	# Move birds that are about to colide
-	velocities += np.sum(separate_if_collide,1)
+		boids_middle=np.mean(self.positions, 1)
+		direction_boids_middle = self.positions-boids_middle[:, np.newaxis]
+		self.velocities -= direction_boids_middle * self.config['move_to_middle_strength']
 
-# Try to match speed with nearby boids
-def match_speed(positions, velocities):
+	def fly_away(self):	
 
-    displacements = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
-    squared_displacements = np.power(displacements,2)
-    square_distances = np.sum(squared_displacements, 0)	
-    velocity_differences = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
-    too_far=square_distances > config['boids_flying_distance']
-    velocity_differences_if_close = np.copy(velocity_differences)
-    velocity_differences_if_close[0,:,:][too_far] = 0
-    velocity_differences_if_close[1,:,:][too_far] = 0
-	# Match speed for those that are going too far
-    velocities -=  np.mean(velocity_differences_if_close, 1) * config['boids_flying_strength']
-				
+		# Compute distances between each boid and the others in a matrix
+		displacements = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
+		squared_displacements = np.power(displacements,2)
+		square_distances = np.sum(squared_displacements, 0)
+		too_far = square_distances > self.config['alert_distance']
+		separate_if_collide = np.copy(displacements)
+		separate_if_collide[0,:,:][too_far] = 0
+		separate_if_collide[1,:,:][too_far] = 0
+		# Move birds that are about to colide
+		self.velocities += np.sum(separate_if_collide,1)
 
-# Update position
-def update_position(positions, velocities):
-	positions+=velocities
+	def match_speed(self):
 
-def update_boids(positions, velocities):
+		displacements = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
+		squared_displacements = np.power(displacements,2)
+		square_distances = np.sum(squared_displacements, 0)	
+		velocity_differences = self.velocities[:,np.newaxis,:] - self.velocities[:,:,np.newaxis]
+		too_far=square_distances > self.config['boids_flying_distance']
+		velocity_differences_if_close = np.copy(velocity_differences)
+		velocity_differences_if_close[0,:,:][too_far] = 0
+		velocity_differences_if_close[1,:,:][too_far] = 0
+		# Match speed for those that are going too far
+		self.velocities -=  np.mean(velocity_differences_if_close, 1) * self.config['boids_flying_strength']
 
-	# Fly towards the middle
-  	fly_middle(positions,velocities)
+	def update_position(self):
+		self.positions+=self.velocities
 
- 	# Fly away from nearby boids
-  	fly_away(positions,velocities)
+	def update_boids(self):
 
- 	# Try to match speed with nearby boids
-  	match_speed(positions,velocities)
+		# Fly towards the middle
+	  	self.fly_middle()
 
-	# Move according to velocities
-	update_position(positions,velocities)
+	 	# Fly away from nearby boids
+	  	self.fly_away()
 
-# Initialise flock
-positions = new_formation(config['no_boids'], lower_limits, upper_limits)	
-velocities = new_formation(config['no_boids'], lower_limits_velocities, upper_limits_velocities)
+	 	# Try to match speed with nearby boids
+	  	self.match_speed()
+
+		# Move according to velocities
+		self.update_position()
+
+	def set_positions(self, new_position):
+		self.positions = new_position
+
+	def set_velocities(self, new_velocity):
+		self.velocities = new_velocity
+
+
+# Initialise an object
+boids=Flock('config.yaml')
 
 figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
-scatter=axes.scatter(positions[0,:],positions[1,:])
+scatter=axes.scatter(boids.positions[0,:],boids.positions[1,:])
 
 def animate(frame):
-   # np.column_stack((positions,velocities))
-   update_boids(positions, velocities)
-   scatter.set_offsets(zip(positions[0,:],positions[1,:]))
+ 
+   boids.update_boids()
+   scatter.set_offsets(zip(boids.positions[0,:],boids.positions[1,:]))
 
 
 anim = animation.FuncAnimation(figure, animate,
                                frames=50, interval=50)
 
-
-
 if __name__ == "__main__":
     plt.show()
+
+
